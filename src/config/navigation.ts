437df6a85d0c85c71d7e78628d5788d40216caea
Andrew Akyampong <andrewakyampong@gmail.com>
2025-01-13 17:03:20 +0000

    social logos fix

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -10,12 +10,12 @@ export const socialItems = [
     href: 'https://t.me/sciaofficial',
     label: 'Telegram',
     isExternal: true,
-    icon: '/logos/X.svg',
+    icon: '/logos/telegram.svg',
   }, // Add a Telegram link.com/scia-ai', label: 'GitHub', isExternal: true },
   {
     href: 'https://twitter.com/scia_ai',
     label: 'Twitter',
     isExternal: true,
-    icon: '/logos/telegram.svg',
+    icon: '/logos/X.svg',
   },
 ] as const;
