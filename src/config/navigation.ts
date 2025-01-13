2a18acbea6c132374bd6c1fa9a1c9310e4f76ca5
Andrew Akyampong <andrewakyampong@gmail.com>
2025-01-13 19:35:49 +0000

    Removing use cases from navigation links

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -1,8 +1,11 @@
 export const navigationItems = [
   { href: '#about', label: 'About' },
   { href: '#features', label: 'Features' },
-  { href: '#use-cases', label: 'Use Cases' },
-  { href: 'https://scia.gitbook.io/scia/', label: 'Docs', isExternal: true },
+  {
+    href: 'https://scia.gitbook.io/litepaper/',
+    label: 'Docs',
+    isExternal: true,
+  },
 ] as const;
 
 export const socialItems = [
